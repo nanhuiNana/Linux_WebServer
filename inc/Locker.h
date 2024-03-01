6574b676dc561e74fe922b3c2e4fb21d05e5bb90
nanhui <3011940542@qq.com>
2024-03-01 11:33:48 +0800

    封装互斥量，添加readme文档

--- inc/Locker.h
+++ inc/Locker.h
@@ -1,33 +1,67 @@
 #ifndef __LOCKER_H__
 #define __LOCKER_H__
 
-#include<semaphore.h>
-#include<stdio.h>
-#include<errno.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /*封装信号量*/
-class Sem{
+class Sem {
 private:
     sem_t mySem;
+
 public:
-    Sem(){
-        if(sem_init(&mySem,0,1)==-1){
-            perror("Sem error");
+    Sem() {
+        if (sem_init(&mySem, 0, 1) == -1) {
+            perror("sem_init error");
+            exit(-1);
         }
     }
-    Sem(int num){
-        if(sem_init(&mySem,0,num)==-1){
-            perror("Sem_int error");
+    Sem(int num) {
+        if (sem_init(&mySem, 0, num) == -1) {
+            perror("sem_init_int error");
+            exit(-1);
         }
     }
-    ~Sem(){
-        sem_destroy(&mySem);
+    ~Sem() {
+        if (sem_destroy(&mySem) == -1) {
+            perror("sem_destroy error");
+            exit(-1);
+        }
     }
-    bool wait(){
-        return sem_wait(&mySem);
+    bool wait() {
+        return sem_wait(&mySem) == 0;
     }
-    bool post(){
-        return sem_post(&mySem);
+    bool post() {
+        return sem_post(&mySem) == 0;
+    }
+};
+
+/*封装互斥量*/
+class Mutex {
+private:
+    pthread_mutex_t myMutex;
+
+public:
+    Mutex() {
+        if (pthread_mutex_init(&myMutex, NULL) != 0) {
+            perror("mutex_init error");
+            exit(-1);
+        }
+    }
+    ~Mutex() {
+        if (pthread_mutex_destroy(&myMutex) != 0) {
+            perror("mutex destroy error");
+            exit(-1);
+        }
+    }
+    bool lock() {
+        return pthread_mutex_lock(&myMutex) == 0;
+    }
+    bool unlock() {
+        return pthread_mutex_unlock(&myMutex) == 0;
     }
 };
 
