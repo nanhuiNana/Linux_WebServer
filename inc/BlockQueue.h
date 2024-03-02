40c3bb8edcc38e15e5561559b8e6f200878e15b0
nanhui <3011940542@qq.com>
2024-03-02 21:57:22 +0800

    优化代码逻辑

--- inc/BlockQueue.h
+++ inc/BlockQueue.h
@@ -1,15 +1,8 @@
 #ifndef __BLOCKQUEUE_H__
 #define __BLOCKQUEUE_H__
 
-#include <./Locker.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <sys/time.h>
-
-#include <iostream>
-#include <queue>
-using std::queue;
-
+#include "Locker.h"
+#include "wrap.h"
 /*
  * 使用STL中的queue容器实现阻塞队列
  * 使用互斥锁保证线程安全，使用条件变量实现线程同步
@@ -17,16 +10,16 @@ using std::queue;
 template <typename T>
 class BlockQueue {
 private:
-    queue<T> myQueue;  // 任务队列
-    int maxSize;       // 队列长度最大值
-    Mutex myMutex;     // 互斥锁
-    Cond myCond;       // 条件变量
+    queue<T> myQueue; // 任务队列
+    int maxSize;      // 队列长度最大值
+    Mutex myMutex;    // 互斥锁
+    Cond myCond;      // 条件变量
 
 public:
     // 构造方法中初始化成员变量
     BlockQueue(int num = 1000) : maxSize(num) {
         if (maxSize <= 0) {
-            exit(-1);
+            sys_error("BlockQueue maxSize is less than and equal to zero");
         }
     }
 
@@ -43,10 +36,9 @@ public:
         if ((int)myQueue.size() >= maxSize) {
             myMutex.unlock();
             return true;
-        } else {
-            myMutex.unlock();
-            return false;
         }
+        myMutex.unlock();
+        return false;
     }
 
     // 判断队列是否为空
@@ -58,29 +50,27 @@ public:
     }
 
     // 获取队头元素，通过返回值判断是否成功
-    bool front(T& value) {
+    bool front(T &value) {
         myMutex.lock();
         if (myQueue.empty()) {
             myMutex.unlock();
             return false;
-        } else {
-            value = myQueue.front();
-            myMutex.unlock();
-            return true;
         }
+        value = myQueue.front();
+        myMutex.unlock();
+        return true;
     }
 
     // 获取队尾元素，通过返回值判断是否成功
-    bool back(T& value) {
+    bool back(T &value) {
         myMutex.lock();
         if (myQueue.empty()) {
             myMutex.unlock();
             return false;
-        } else {
-            value = myQueue.back();
-            myMutex.unlock();
-            return true;
         }
+        value = myQueue.back();
+        myMutex.unlock();
+        return true;
     }
 
     // 获取队列长度
@@ -102,7 +92,7 @@ public:
     }
 
     // 入队操作，生产者生产产品放入任务队列中
-    bool push(const T& value) {
+    bool push(const T &value) {
         myMutex.lock();
         if ((int)myQueue.size() >= maxSize) {
             myCond.broadcast();
@@ -116,7 +106,7 @@ public:
     }
 
     // 出队操作，消费者从任务队列中获取产品
-    bool pop(T& value) {
+    bool pop(T &value) {
         myMutex.lock();
         while (myQueue.size() <= 0) {
             if (!myCond.wait(myMutex.getMyMutex())) {
@@ -131,7 +121,7 @@ public:
     }
 
     // 出队操作，给工作线程添加超时时长
-    bool pop(T& value, int timeoutMS) {
+    bool pop(T &value, int timeoutMS) {
         struct timespec timeout = {0, 0};
         struct timeval now = {0, 0};
         gettimeofday(&now, NULL);
