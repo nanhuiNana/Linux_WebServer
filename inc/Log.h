646e7d0d593dc351c0d79ad1c585dce09d374e3a
nanhui <3011940542@qq.com>
2024-03-02 21:58:03 +0800

    优化头文件

--- inc/Log.h
+++ inc/Log.h
@@ -1,16 +1,9 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <string>
-
 #include "BlockQueue.h"
 #include "Locker.h"
-using std::string;
+#include "wrap.h"
 
 // Log分级对外宏定义
 #define LOG_DEBUG(format, ...) Log::getInstance()->writeLog(0, format, ##__VA_ARGS__)
@@ -29,30 +22,30 @@ private:
     void asyncWriteLog();
 
 private:
-    char myDirName[128];             // 路径名
-    char myLogName[128];             // 日志文件名
-    FILE* myLogFilePointer;          // 打开日志文件的指针
-    long long myLogCount;            // 记录日志行数
-    int myLogMaxLines;               // 日志最大行数
-    int myToday;                     // 记录当前时间是哪一天
-    char* myLogBuf;                  // 日志缓冲区
-    int myLogBufSize;                // 日志缓冲区大小
-    bool myIsAsync;                  // 异步开关
-    BlockQueue<string>* myLogQueue;  // 日志队列
-    Mutex myMutex;                   // 互斥锁
+    char myDirName[128];            // 路径名
+    char myLogName[128];            // 日志文件名
+    FILE *myLogFilePointer;         // 打开日志文件的指针
+    long long myLogCount;           // 记录日志行数
+    int myLogMaxLines;              // 日志最大行数
+    int myToday;                    // 记录当前时间是哪一天
+    char *myLogBuf;                 // 日志缓冲区
+    int myLogBufSize;               // 日志缓冲区大小
+    bool myIsAsync;                 // 异步开关
+    BlockQueue<string> *myLogQueue; // 日志队列
+    Mutex myMutex;                  // 互斥锁
 
 public:
     // 创建一个公有静态方法获得实例，使用指针进行返回，避免不必要的拷贝构造
-    static Log* getInstance();
+    static Log *getInstance();
 
     // 初始化日志文件名、日志缓冲区大小、日志最大行数、日志队列最大长度
-    bool init(const char* fileName, int logBufSize = 8192, int logMaxLines = 5000000, int queueMaxSize = 0);
+    bool init(const char *fileName, int logBufSize = 8192, int logMaxLines = 5000000, int queueMaxSize = 0);
 
     // 日志写入线程回调函数
-    static void* logWritePthreadCallback(void* args);
+    static void *logWritePthreadCallback(void *args);
 
     // 日志写入主函数
-    void writeLog(int level, const char* format, ...);
+    void writeLog(int level, const char *format, ...);
 };
 
 #endif
